5aa316f7bb4d2b08eff66fef0856228b974c5f18
Teerawat chuaphanngam <teerawat2545@gmail.com>
2023-11-20 02:24:37 +0700

    updated main.go add required rest api

--- Premier League Scouting Service/main.go
+++ Premier League Scouting Service/main.go
@@ -3,71 +3,130 @@ package main
 import (
     "fmt"
     "net/http"
-	"log"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
     "github.com/gorilla/mux"
 )
 
+var db *sql.DB
+var err error
+
 func main() {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
-
-    r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-        vars := mux.Vars(r)
-        title := vars["title"]
-        page := vars["page"]
-
-        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
-    })
-
-	// Create a subrouter for static files
-    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
-    // staticRouter.PathPrefix("/css/").Handler(http.StripPrefix("/static/css/", http.FileServer(http.Dir("static/css/"))))
-
-	// Configure the database connection (always check errors)
-	db, err := sql.Open("mysql", "root:root@(127.0.0.1:3306)/siit?parseTime=true")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Initialize the first connection to the database, to see if everything works correctly.
-	// Make sure to check the error.
-	if err = db.Ping(); err != nil {
-		log.Fatal(err)
-	}
-
-	http.Handle("/", r)
-	http.ListenAndServe(":80", r)
+    Routers()
 }
 
+func Routers(){
+    // InitDB()
+    // get all fixtures for a given gameweek
+    r := mux.NewRouter()
+    r.HandleFunc("/fixtures/{gw}", GetFixtures).Methods("GET")
+    // get, post, and update match info
+    r.HandleFunc("/matchinfo/{id}", PostMatchInfo).Methods("POST")
+    r.HandleFunc("/matchinfo/{id}", GetMatchInfo).Methods("GET")
+    r.HandleFunc("/matchinfo/{id}", UpdateMatchInfo).Methods("PUT")
+    // get player available for a given gameweek
+    r.HandleFunc("/players/{teamId}", GetPlayers).Methods("GET")
+    // get and post player for each game lineup
+    r.HandleFunc("/lineup/{gw}/{teamId}", PostLineup).Methods("POST")
+    r.HandleFunc("/lineup/{gw}/{teamId}", GetLineup).Methods("GET")
+    // get, post, update and delete match events
+    r.HandleFunc("/matchevents/{matchinfoId}", GetMatchEvents).Methods("GET")
+    r.HandleFunc("/matchevents/{matchinfoId}", PostMatchEvents).Methods("POST")
+    r.HandleFunc("/matchevents/{matchinfoId}/{matcheventId}", UpdateMatchEvents).Methods("PUT")
+    r.HandleFunc("/matchevents/{matchinfoId}/{matcheventId}", DeleteMatchEvents).Methods("DELETE")
 
-
-
-
-
-// CORSRouterDecorator applies CORS headers to a mux.Router
-type CORSRouterDecorator struct {
-    R *mux.Router
+    http.ListenAndServe(":80", r)
 }
 
-func (c *CORSRouterDecorator) ServeHTTP(rw http.ResponseWriter,
-    req *http.Request) {
-    if origin := req.Header.Get("Origin"); origin != "" {
-        rw.Header().Set("Access-Control-Allow-Origin", origin)
-        rw.Header().Set("Access-Control-Allow-Methods",
-            "POST, GET, OPTIONS, PUT, DELETE")
-        rw.Header().Set("Access-Control-Allow-Headers",
-            "Accept, Accept-Language,"+
-                " Content-Type, YourOwnHeader")
+func GetFixtures(w http.ResponseWriter, r *http.Request) {
+    // get all fixtures for a given gameweek
+    vars := mux.Vars(r)
+    gw := vars["gw"]
+    db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/eplscout")
+    if err != nil {
+        panic(err.Error())
     }
-    // Stop here if its Preflighted OPTIONS request
-    if req.Method == "OPTIONS" {
-        return
-    }
+    defer db.Close()
 
-    c.R.ServeHTTP(rw, req)
+    fmt.Fprintf(w, "You've requested the gameweek: %s\n", gw)
 }
+
+func PostMatchInfo(w http.ResponseWriter, r *http.Request) {
+    // post match info
+    vars := mux.Vars(r)
+    id := vars["id"]
+    fmt.Fprintf(w, "You've requested the match info: %s\n", id)
+}
+
+func GetMatchInfo(w http.ResponseWriter, r *http.Request) {
+    // get match info
+    vars := mux.Vars(r)
+    id := vars["id"]
+    fmt.Fprintf(w, "You've requested the match info: %s\n", id)
+}
+
+func UpdateMatchInfo(w http.ResponseWriter, r *http.Request) {
+    // update match info
+    vars := mux.Vars(r)
+    id := vars["id"]
+    fmt.Fprintf(w, "You've requested the match info: %s\n", id)
+}
+
+func GetPlayers(w http.ResponseWriter, r *http.Request) {
+    // get player available for a given gameweek
+    vars := mux.Vars(r)
+    teamId := vars["teamId"]
+    fmt.Fprintf(w, "You've requested the team: %s\n", teamId)
+}
+
+func PostLineup(w http.ResponseWriter, r *http.Request) {
+    // post player for each game lineup
+    vars := mux.Vars(r)
+    gw := vars["gw"]
+    teamId := vars["teamId"]
+    fmt.Fprintf(w, "You've requested the gameweek: %s\n", gw)
+    fmt.Fprintf(w, "You've requested the team: %s\n", teamId)
+}
+
+func GetLineup(w http.ResponseWriter, r *http.Request) {
+    // get player for each game lineup
+    vars := mux.Vars(r)
+    gw := vars["gw"]
+    teamId := vars["teamId"]
+    fmt.Fprintf(w, "You've requested the gameweek: %s\n", gw)
+    fmt.Fprintf(w, "You've requested the team: %s\n", teamId)
+}
+
+func GetMatchEvents(w http.ResponseWriter, r *http.Request) {
+    // get match events
+    vars := mux.Vars(r)
+
+    matchinfoId := vars["matchinfoId"]
+    fmt.Fprintf(w, "You've requested the match info: %s\n", matchinfoId)
+}
+
+func PostMatchEvents(w http.ResponseWriter, r *http.Request) {
+    // post match events
+    vars := mux.Vars(r)
+    matchinfoId := vars["matchinfoId"]
+    fmt.Fprintf(w, "You've requested the match info: %s\n", matchinfoId)
+}
+
+func UpdateMatchEvents(w http.ResponseWriter, r *http.Request) {
+    // update match events
+    vars := mux.Vars(r)
+    matchinfoId := vars["matchinfoId"]
+    matcheventId := vars["matcheventId"]
+    fmt.Fprintf(w, "You've requested the match info: %s\n", matchinfoId)
+    fmt.Fprintf(w, "You've requested the match event: %s\n", matcheventId)
+}
+
+func DeleteMatchEvents(w http.ResponseWriter, r *http.Request) {
+    // delete match events
+    vars := mux.Vars(r)
+    matchinfoId := vars["matchinfoId"]
+    matcheventId := vars["matcheventId"]
+    fmt.Fprintf(w, "You've requested the match info: %s\n", matchinfoId)
+    fmt.Fprintf(w, "You've requested the match event: %s\n", matcheventId)
+}
+
