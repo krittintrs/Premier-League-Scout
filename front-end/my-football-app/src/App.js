4a20bf370a7bb86a2fcd97ab3b534fa99e973938
phukaokub <teerawat2545@gmail.com>
2023-11-24 15:56:26 +0700

    add new file

--- front-end/my-football-app/src/App.js
+++ front-end/my-football-app/src/App.js
@@ -8,23 +8,35 @@ import StackTable from "./components/FixturePage/StackTable";
 import BottomDiv from './components/FixturePage/BottomDiv';
 import MatchCard from './components/FixturePage/MatchCard';
 import DetailsPage from './components/MatchDetails/DetailsPage';
+import fixturePage from './components/Pages/fixturePage';
 
 function App() {
-  const handleClick = () => {
-    console.log('Button clicked!');
-  };
-
-
   return (
-    <div className="w-full md:w-[350px] lg:w-[800px] m-auto">
-        <Header />
-        <SubHeader />
-        <CalendarItem label="Click me" onClick={handleClick} />   
-        <StackTable/>  
-        <BottomDiv />
+    <Router>
+            <Routes>
+                <Route exact path="/" element={<fixturePage />} />
+                <Route path="/about" element={<fixturePage />} />
+                <Route
+                    path="/contact"
+                    element={<fixturePage />}
+                />
+                <Route path="/blogs" element={<fixturePage />} />
+                <Route
+                    path="/sign-up"
+                    element={<fixturePage />}
+                />
+            </Routes>
+        </Router>
+    );
+  //   <div className="w-full md:w-[350px] lg:w-[800px] m-auto">
+  //       <Header />
+  //       <SubHeader />
+  //       <CalendarItem label="Click me" onClick={handleClick} />   
+  //       <StackTable/>  
+  //       <BottomDiv />
       
-    </div>
-  );
+  //   </div>
+  // );
 }
 
 export default App;
