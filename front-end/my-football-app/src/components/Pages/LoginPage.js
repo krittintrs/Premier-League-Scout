817477dca0fb44700d6145aa5c927547cd698586
Krittin Thirasak <krittin.thirasak@gmail.com>
2023-11-24 15:57:08 +0700

    add login page

---
+++ front-end/my-football-app/src/components/Pages/LoginPage.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const LoginPage = () => {
+  // State for keeping track of the username and password
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Basic validation
+    if (!username || !password) {
+      alert('Please enter both username and password.');
+      return;
+    }
+
+    // Perform authentication (you would replace this with your actual authentication logic)
+    // For simplicity, let's just log the username to the console
+    console.log(`Logged in as: ${username}`);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
