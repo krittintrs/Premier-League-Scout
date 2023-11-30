15fa0339f4a4ba7ca566bc67cd616f4303b160a7
Paweena Kanokhong <bbewbpk@gmail.com>
2023-12-01 00:12:12 +0700

    idk

--- front-end/my-football-app/src/components/Pages/ScoreBoard.js
+++ front-end/my-football-app/src/components/Pages/ScoreBoard.js
@@ -1,125 +1,54 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { CLUB_LIST } from "../Services/Actions/ClubAction";
-import Table from "../Table";
-import { clubListAction } from "../Services/Actions/clubListAction";
+import React from 'react';
 
+const Scoreboard = () => {
+  const sampleData = [
+    { id: 1, name: 'Manchester United', wins: 7, draws: 2, losses: 1 },
+    { id: 2, name: 'Manchester City', wins: 6, draws: 3, losses: 1 },
+    { id: 3, name: 'Liverpool', wins: 5, draws: 4, losses: 1 },
+    { id: 4, name: 'Chelsea', wins: 5, draws: 3, losses: 2 },
+    { id: 5, name: 'Arsenal', wins: 4, draws: 4, losses: 2 },
+    { id: 6, name: 'Tottenham Hotspur', wins: 4, draws: 3, losses: 3 },
+    { id: 7, name: 'Leicester City', wins: 3, draws: 5, losses: 2 },
+    { id: 8, name: 'West Ham United', wins: 3, draws: 4, losses: 3 },
+    { id: 9, name: 'Everton', wins: 3, draws: 3, losses: 4 },
+    { id: 10, name: 'Leeds United', wins: 2, draws: 2, losses: 6 },
+  ];
 
-const ScoreBoard = () => {
-  const clubData = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(CLUB_LIST());
-  }, [dispatch]);
-
-  var matches = clubData?.ClubData?.matches ?? [];
-
-  if (matches.length > 0) {
-    matches = matches.filter((x) => x.score);
-  }
-
-  let firstTeam = [];
-  let secondTeam = [];
-
-  for (let i = 0; i < matches.length; i++) {
-    let score1 = matches[i].score.ft[0];
-    let score2 = matches[i].score.ft[1];
-    firstTeam.push({
-      name: matches[i].team1,
-      played: 1,
-      won: score1 > score2 ? 1 : 0,
-      lost: score1 < score2 ? 1 : 0,
-      draw: score1 == score2 ? 1 : 0,
-      points: score1 > score2 ? 3 : score1 == score2 ? 1 : 0,
-      date: matches[i].date,
-    });
-
-    secondTeam.push({
-      played: 1,
-      name: matches[i].team2,
-      won: score2 > score1 ? 1 : 0,
-      lost: score2 < score1 ? 1 : 0,
-      draw: score1 == score2 ? 1 : 0,
-      points: score2 > score1 ? 3 : score1 == score2 ? 1 : 0,
-      date: matches[i].date,
-    });
-  }
-
-  let combinedTeam = [...firstTeam, ...secondTeam].sort(function (a, b) {
-    return (
-      a?.date?.split("-")?.join(",")?.replace(/,/g, "") -
-      b?.date?.split("-")?.join(",")?.replace(/,/g, "")
-    );
-  });
-
-  var result = [];
-
-  combinedTeam.reduce(function (res, value) {
-    if (!res[value.name]) {
-      res[value.name] = {
-        name: value.name,
-        played: 0,
-        won: 0,
-        lost: 0,
-        draw: 0,
-        points: 0,
-        form: [],
-      };
-      result.push(res[value.name]);
-    }
-
-    res[value.name].played += value.played;
-    res[value.name].won += value.won;
-    res[value.name].lost += value.lost;
-    res[value.name].draw += value.draw;
-    res[value.name].points += value.points;
-    if (res[value.name].form.length == 5) {
-      res[value.name].form.shift();
-    }
-    res[value.name].form.push(value.points);
-
-    return res;
-  }, {});
-
-  const [isOpen, setIsOpen] = useState({
-    show: false,
-    id: null,
-  });
- 
-  useEffect(() => {
-    let mounted = true;
-    if (mounted) {
-      dispatch(CLUB_LIST());
-    }
-    return () => {
-      mounted = false;
-    };
-  }, []);
-
-  const sortedClub = Object.values(result).sort(function (a, b) {
-    return b.points - a.points;
-  });
+  const calculatePoints = (wins, draws) => wins * 3 + draws;
 
   return (
-    <>
-      <div className="container">
-        <div className="title">
-          <img src={"/images/logo.png"} alt={clubData?.ClubData?.name} />
-          <h1>{clubData?.ClubData?.name}</h1>
+    <div>
+      {/* Header */}
+      <div style={{ display: 'flex', padding: '8px', justifyContent: 'space-between', borderBottom: '1px solid #ccc' }}>
+      <div style={{ width: '50%' }}>Rank</div>
+        <div style={{ width: '50%' }}>Club</div>
+        <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', width: '50%' }}>
+          <div style={{ width: 60, textAlign: 'center' }}>W</div>
+          <div style={{ width: 60, textAlign: 'center' }}>D</div>
+          <div style={{ width: 60, textAlign: 'center' }}>L</div>
+          <div style={{ width: 60, textAlign: 'center' }}>Point</div>
+          
         </div>
-
-        <table className="table">
-            clubData={sortedClub}
-          
-          
-        </table>
       </div>
 
-      
-      
-    </>
+      {/* Match Data */}
+      <ul style={{ padding: 0 }}>
+        {sampleData.map((team, index) => (
+          <li key={index} style={{ listStyle: 'none', borderBottom: '1px solid #ccc', display: 'flex', padding: '8px' }}>
+             <div style={{ width: '50%' }}>{team.id}</div>
+            <div style={{ width: '50%' }}>{team.name}</div>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', width: '50%' }}>
+              <div style={{ width: 60, textAlign: 'center' }}>{team.wins}</div>
+              <div style={{ width: 60, textAlign: 'center' }}>{team.draws}</div>
+              <div style={{ width: 60, textAlign: 'center' }}>{team.losses}</div>
+              <div style={{ width: 60, textAlign: 'center' }}>{calculatePoints(team.wins, team.draws)}</div>
+             
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
-export default ScoreBoard;
+export default Scoreboard;
