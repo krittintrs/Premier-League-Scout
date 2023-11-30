b1848084efc3148468ca61d996a60a349c2bd905
Paweena Kanokhong <bbewbpk@gmail.com>
2023-11-30 20:50:07 +0700

    navbar

--- front-end/my-football-app/src/App.js
+++ front-end/my-football-app/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import "./App.css";
+import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import FixturePage from './components/Pages/fixturePage';
 import MatchDetails from './components/Pages/MatchDetails';
 import AddMatchEvent from './components/Pages/AddMatchEvent';
 import LineUp from './components/Pages/LineUp';
 import ScoreBoard from './components/Pages/ScoreBoard';
+import NavBar from './components/NavBar';
+import LoginPage from './components/Pages/LoginPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<FixturePage />} />
-      <Route path="/MatchDetails" element={<MatchDetails />} />
-      <Route path="/AddMatchEvent" element={<AddMatchEvent/>} />
-      <Route path="/LineUp" element={<LineUp/>} />
-      <Route path="/ScoreBoard" element={<ScoreBoard/>} />
-    </Routes>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<FixturePage />} />
+        <Route path="/MatchDetails" element={<MatchDetails />} />
+        <Route path="/AddMatchEvent" element={<AddMatchEvent />} />
+        <Route path="/LineUp" element={<LineUp/>} />
+        <Route path="/ScoreBoard" element={<ScoreBoard />} />
+        <Route path="/LoginPage" element={<LoginPage />} />
+      </Routes>
+    </div>
   );
 }
 
--- front-end/my-football-app/src/components/NavBar.js
+++ front-end/my-football-app/src/components/NavBar.js
@@ -1,28 +1,59 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import Home from '@mui/icons-material/Home';
+import Person from '@mui/icons-material/Person';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const NavBar = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li><a>Link</a></li>
-          <li>
-            <details>
-              <ul className="p-2 bg-base-100 rounded-t-none">
-                <li><a>Link 1</a></li>
-                <li><a>Link 2</a></li>
-                <li><a>Link 3</a></li>
-                <li><a>Link 4</a></li>
-              </ul>
-            </details>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <Box component="nav" aria-label="My site" sx={{ flexGrow: 1 }}>
+      <List role="menubar" sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <ListItem role="none">
+        <Home/>
+          <ListItemButton
+            role="menuitem"
+            component="a"
+            href="#FixTurePage"
+            aria-label="Home"
+            
+          > Premier-League-Scout
+            
+          </ListItemButton>
+        </ListItem>
+        <Divider orientation="vertical" flexItem />
+        <ListItem role="none">
+          <ListItemButton role="menuitem" component="a" href="#LineUP">
+          LineUP
+          </ListItemButton>
+        </ListItem>
+        <Divider orientation="vertical" flexItem />
+      
+        <ListItem role="none">
+          <ListItemButton role="menuitem" component="a" href="#blog">
+            ScoreBoard
+          </ListItemButton>
+        </ListItem>
+        
+        <Divider orientation="vertical" flexItem />
+        <Link to="/LoginPage">
+        <ListItem role="none">
+          <ListItemButton
+            role="menuitem"
+            component="a"
+            href="#profile"
+            aria-label="Profile"
+          >
+            <Person />
+          </ListItemButton>
+        </ListItem>
+        </Link>
+      </List>
+    </Box>
   );
 };
 
-export default Navbar;
+export default NavBar;
